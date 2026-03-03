43f3ecd41b7adc3ce3bd7984d70204a38e970b1d
Kauan Tillmann <kauantillmann53@gmail.com>
2026-03-02 23:34:19 -0300

    fix: adicionando base path para github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Define o caminho base como o nome do repositório no GitHub
+  base: "/kauan-s-digital-realm/",
   server: {
     host: "::",
     port: 8080,
